684e0302db8c3070eabe0391b4574e3330109189
alberto batista <albertomimi16@gmail.com>
2025-02-26 18:59:03 -0400

    Add dataset of value names array

--- aform.js
+++ aform.js
@@ -7,13 +7,15 @@ window.onload = () => {
             form_temp.setAttribute('method', e.target.dataset.method);
             form_temp.setAttribute('action', e.target.dataset.action);
             form_temp.style.display = 'none';
-
-            let input_temp = document.createElement('input');
-            input_temp.setAttribute('name', e.target.dataset.name);
-            input_temp.setAttribute('value', e.target.dataset.value);
-            input_temp.setAttribute('type', e.target.dataset.type);
-
-            form_temp.appendChild(input_temp);
+            let data_aform = JSON.parse(e.target.dataset.value);
+            for (let key in data_aform) {
+                let input_temp = document.createElement('input');
+                input_temp.setAttribute('name', key);
+                input_temp.setAttribute('value', data_aform[key]);
+                input_temp.setAttribute('type', 'text');
+                form_temp.appendChild(input_temp);
+            }
+            
             document.body.appendChild(form_temp);
             form_temp.submit();
         }
